355573e760ba0434b18f7ca27eb939f431096968
ngtnh013 <tuananh.ngtnh@gmail.com>
2024-06-29 16:38:44 +0700

    Add Damage Feedback

--- Assets/Scripts/Enemy/EnemyMovement.cs
+++ Assets/Scripts/Enemy/EnemyMovement.cs
@@ -6,6 +6,10 @@ public class EnemyMovement : MonoBehaviour
 {
     EnemyStats enemy;
     Transform player;
+
+    Vector2 knockbackVelocity;
+    float knockbackDuration;
+
     void Start()
     {
         enemy = GetComponent<EnemyStats>();
@@ -14,6 +18,22 @@ public class EnemyMovement : MonoBehaviour
 
     void Update()
     {
-        transform.position = Vector2.MoveTowards(transform.position, player.transform.position, enemy.currentMoveSpeed * Time.deltaTime);
+        if (knockbackDuration > 0)
+        {
+            transform.position += (Vector3)knockbackVelocity * Time.deltaTime;
+            knockbackDuration -= Time.deltaTime;
+        }
+        else
+        {
+            transform.position = Vector2.MoveTowards(transform.position, player.transform.position, enemy.currentMoveSpeed * Time.deltaTime);
+        }
+    }
+
+    public void Knockback(Vector2 velocity, float duration)
+    {
+        if (knockbackDuration > 0) return;
+
+        knockbackVelocity = velocity;
+        knockbackDuration = duration;
     }
 }
